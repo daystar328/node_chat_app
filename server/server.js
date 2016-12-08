c434164def1c1840e1c4eada226bf19e79bbbc5f
David Chang(張志堅) <David_Chang@HTC.COM>
2016-12-08 19:40:19 +0800

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname,'../public');
 var port = process.env.PORT || 3000;
 var app = express();
-
+var server = http.createServer(app)
+var io = socketIO(server); //socketIO listen to the server
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', ()=>{
+    console.log('User was disconnected');
+  });
+});
 
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server running on port ${port}`);
 })

b1cce5a0adc17a27fb58e2c2190c4166eab9fc09
David Chang(張志堅) <David_Chang@HTC.COM>
2016-12-09 17:30:03 +0800

    add chat form

--- public/js/index.js
+++ public/js/index.js
@@ -11,10 +11,31 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 })
 
 
 
 socket.on('newEmail', function(email) {
   console.log('New email', email);
+});
+
+socket.emit('createMessage', {
+  from:'Tom',
+  text:'Hi'
+}, function(data) {
+  console.log('Got it', data);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    form: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
